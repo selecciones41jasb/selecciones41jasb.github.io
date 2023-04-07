fa64a8e5944ec0bc63f1aedbe1d5b6015a5456b0
selecciones41jasb <selecciones41jasb@gmail.com>
2023-04-07 14:19:56 -0600

    Cambioo

---
+++ init.js
@@ -0,0 +1,10 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyDlVv7h6nNbS8lfOh3lRyiFQ8cGQOVGq18",
+    authDomain: "selecciones41jasb1.firebaseapp.com",
+    projectId: "selecciones41jasb1",
+    storageBucket: "selecciones41jasb1.appspot.com",
+    messagingSenderId: "281503044178",
+    appId: "1:281503044178:web:e67fc8d8e9c85b2d0f458b",
+    measurementId: "G-ZWYHRQJKK9"
+       });
+   
